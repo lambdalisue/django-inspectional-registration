2d98a02310a36417b76b23c4a93ece4555cdf482
Λlisue <lambdalisue@hashnote.net>
2014-08-17 02:10:46 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages, Command
 from setuptools.command.sdist import sdist as original_sdist
 
 NAME = 'django-inspectional-registration'
-VERSION = '0.4.2'
+VERSION = '0.4.3'
 
 
 class compile_docs(Command):
