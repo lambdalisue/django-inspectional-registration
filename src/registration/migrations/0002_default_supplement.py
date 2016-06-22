880d42d130e7fd3381b4d704b9b2cf5fceb108d2
lambdalisue <lambdalisue@hashnote.net>
2016-06-22 09:06:52 +0900

    Allow migration on DefaultRegistrationSupplement only when it is specified

---
+++ src/registration/migrations/0002_default_supplement.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+    operations = []
+
+    if settings.REGISTRATION_SUPPLEMENT_CLASS == 'registration.supplements.default':
+        operations += [
+            migrations.CreateModel(
+                name='DefaultRegistrationSupplement',
+                fields=[
+                    ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                    ('remarks', models.TextField(verbose_name='remarks')),
+                ],
+                options={
+                },
+                bases=(models.Model,),
+            ),
+            migrations.AddField(
+                model_name='defaultregistrationsupplement',
+                name='registration_profile',
+                field=models.OneToOneField(editable=False, related_name='_registration_defaultregistrationsupplement_supplement', verbose_name='registration profile', to='registration.RegistrationProfile'),
+                preserve_default=True,
+            ),
+        ]
+
